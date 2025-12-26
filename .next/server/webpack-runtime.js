ff1b7ce53141dbcf320f8e85acf1df4c959e782e
chunitbansal.29 <chunitbansal.29@gmail.com>
2025-12-26 21:41:49 +0000

    Fix dependency conflicts for Vercel deployment

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -30,7 +30,7 @@
 /******/ 		// Execute the module function
 /******/ 		var threw = true;
 /******/ 		try {
-/******/ 			__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 			__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/ 			threw = false;
 /******/ 		} finally {
 /******/ 			if(threw) delete __webpack_module_cache__[moduleId];
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("3cb834dfb81c9d7a")
+/******/ 		__webpack_require__.h = () => ("28e5c44131a79b1e")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
